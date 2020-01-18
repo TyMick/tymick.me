04b5f34d4d58e817beed71883f0a595183a355b9
Ty Mick <ty@tymick.me>
2020-01-18 15:53:45 -0600

    Create main pages

---
+++ pages/contact.jsx
@@ -0,0 +1,15 @@
+"use strict";
+
+import React from "react";
+import Head from "next/head";
+import "../styles.scss";
+
+const Home = () => (
+  <>
+    <Head>
+      <title>Ty Mick</title>
+    </Head>
+  </>
+);
+
+export default Home;
