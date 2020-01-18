fd37584d002dc98404d6409d77eaf1b0da77d9ba
Ty Mick <ty@tymick.me>
2020-01-18 15:51:45 -0600

    Extract template to custom _app

---
+++ pages/_app.jsx
@@ -0,0 +1,40 @@
+"use strict";
+
+import React from "react";
+import Head from "next/head";
+import Nav from "../components/nav";
+
+export default ({ Component, pageProps }) => {
+  return (
+    <>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link
+          href="https://fonts.googleapis.com/css?family=Dawning+of+a+New+Day&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
+      <Nav />
+
+      <Component {...pageProps} />
+    </>
+  );
+};
