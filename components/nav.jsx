6f27c0b9849611eff2e0c6a87f2f06ac4b5310b1
Ty Mick <ty@tymick.me>
2020-01-18 15:42:50 -0600

    Add navbar

---
+++ components/nav.jsx
@@ -0,0 +1,36 @@
+"use strict";
+
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { Container, Navbar, Nav } from "react-bootstrap";
+
+const links = [
+  { label: "Portfolio", href: "/portfolio" },
+  { label: "Writing", href: "/writing" },
+  { label: "Contact", href: "/contact" },
+  { label: "Social", href: "/social" }
+];
+
+export default () => {
+  const router = useRouter();
+  return (
+    <Container>
+      <Navbar collapseOnSelect expand="sm">
+        <Link href="/" passHref>
+          <Navbar.Brand className="signature">Ty Mick</Navbar.Brand>
+        </Link>
+        <Navbar.Toggle aria-controls="nav-links" className="border-0" />
+        <Navbar.Collapse id="nav-links" className="justify-content-end">
+          <Nav activeKey={router.pathname}>
+            {links.map(({ label, href }) => (
+              <Link href={href} passHref key={label.toLowerCase()}>
+                <Nav.Link>{label}</Nav.Link>
+              </Link>
+            ))}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </Container>
+  );
+};
