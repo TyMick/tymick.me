dd026fe26f3317fbab80054d211aaad0c6a2e8c1
Ty Mick <ty@tymick.me>
2020-01-20 11:06:59 -0600

    Add profile pic to navbar on most pages

--- components/nav.jsx
+++ components/nav.jsx
@@ -3,7 +3,7 @@
 import React from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { Container, Navbar, Nav } from "react-bootstrap";
+import { Container, Navbar, Nav, Image } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
@@ -19,20 +19,32 @@ export default () => {
     <Container as="header" className="mb-3 mb-sm-5">
       <Navbar collapseOnSelect expand="sm">
         <Link href="/" passHref>
-          <Navbar.Brand className="signature mr-auto">Ty Mick</Navbar.Brand>
+          <Navbar.Brand href="/" className="signature">
+            {router.pathname !== "/" && (
+              <Image
+                src="/profile-pic.jpg"
+                alt="Ty hiking in Joshua Tree National Park"
+                roundedCircle
+              />
+            )}
+            Ty Mick
+          </Navbar.Brand>
         </Link>
         <Navbar.Toggle aria-controls="nav-links" className="border-0" />
         <Navbar.Collapse id="nav-links" className="justify-content-end">
-          <Nav activeKey={router.pathname}>
+          <Nav activeKey={router.pathname} className="align-items-center">
             {links.map(({ label, href }) => (
               <Link href={href} passHref key={label.toLowerCase()}>
                 <Nav.Link>{label}</Nav.Link>
               </Link>
             ))}
+            <Nav.Link
+              className="github-nav-link"
+              href="https://github.com/TyWMick"
+            >
+              <FontAwesomeIcon icon={faGithub} />
+            </Nav.Link>
           </Nav>
-          <Nav.Link className="github-link" href="https://github.com/TyWMick">
-            <FontAwesomeIcon icon={faGithub} />
-          </Nav.Link>
         </Navbar.Collapse>
       </Navbar>
     </Container>
