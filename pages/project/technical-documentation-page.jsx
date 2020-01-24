5e55b9bae27f2ae9a75532de86c96b90f901a076
Ty Mick <ty@tymick.me>
2020-01-24 12:22:06 -0600

    Add Technical documentation page demo

---
+++ pages/project/technical-documentation-page.jsx
@@ -0,0 +1,142 @@
+"use strict";
+
+import React, { useEffect, useState } from "react";
+import Head from "next/head";
+import { Container } from "react-bootstrap";
+import CodePen from "../../components/codepen";
+
+export default () => {
+  // Just capture the window height once
+  const [windowHeight, setWindowHeight] = useState(0);
+  useEffect(() => {
+    setWindowHeight(window.innerHeight);
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>Technical documentation page – Ty Mick</title>
+      </Head>
+
+      <Container fluid className="mt-n3 mb-4 mb-lg-5">
+        <CodePen
+          title="Technical documentation page"
+          slug="EJrmvx"
+          height={windowHeight * 0.6}
+        />
+      </Container>
+
+      {/* Explanation */}
+      <Container>
+        <p className="lead font-italic">
+          View this app{" "}
+          <a href="https://codepen.io/tywmick/full/EJrmvx">in its own window</a>
+          , or view the source on{" "}
+          <a href="https://github.com/tywmick/technical-documentation-page">
+            GitHub
+          </a>{" "}
+          or <a href="https://codepen.io/tywmick/pen/EJrmvx">CodePen</a>.
+        </p>
+
+        <h1>Technical documentation page</h1>
+
+        <p>
+          I created this app in fulfillment of{" "}
+          <a href="https://www.freecodecamp.org/">freeCodeCamp</a>
+          's Responsive Web Design Project{" "}
+          <a href="https://www.freecodecamp.org/learn/responsive-web-design/responsive-web-design-projects/build-a-technical-documentation-page/">
+            Build a Technical Documentation Page
+          </a>
+          , using{" "}
+          <a href="https://www.w3schools.com/w3css/default.asp">W3.CSS</a>. I
+          modeled it some after{" "}
+          <a href="https://codepen.io/freeCodeCamp/pen/RKRbwL">their example</a>
+          , but I was more inspired more by{" "}
+          <a href="https://www.w3schools.com">W3School</a>'s{" "}
+          <a href="https://www.w3schools.com/w3css/w3css_sidebar.asp">
+            sidebar tutorials
+          </a>
+          . It fulfills the following user stories:
+        </p>
+
+        <ol>
+          <li>
+            I can see a <code>main</code> element with a corresponding{" "}
+            <code>id="main-doc"</code>, which contains the page's main content
+            (technical documentation).
+          </li>
+          <li>
+            Within the <code>#main-doc</code> element, I can see several{" "}
+            <code>section</code> elements, each with a class of{" "}
+            <code>main-section</code>. There should be a minimum of 5.
+          </li>
+          <li>
+            The first element within each <code>.main-section</code> should be a{" "}
+            <code>header</code> element which contains text that describes the
+            topic of that section.
+          </li>
+          <li>
+            Each <code>section</code> element with the class of{" "}
+            <code>main-section</code> should also have an id that corresponds
+            with the text of each <code>header</code> contained within it. Any
+            spaces should be replaced with underscores (e.g. The{" "}
+            <code>section</code> that contains the header "JavaScript and Java"
+            should have a corresponding <code>id="JavaScript_and_Java"</code>).
+          </li>
+          <li>
+            The <code>.main-section</code> elements should contain at least 10{" "}
+            <code>p</code> elements total (not each).
+          </li>
+          <li>
+            The <code>.main-section</code> elements should contain at least 5{" "}
+            <code>code</code> elements total (not each).
+          </li>
+          <li>
+            The <code>.main-section</code> elements should contain at least 5{" "}
+            <code>li</code> items total (not each).
+          </li>
+          <li>
+            I can see a <code>nav</code> element with a corresponding{" "}
+            <code>id="navbar"</code>.
+          </li>
+          <li>
+            The navbar element should contain one <code>header</code> element
+            which contains text that describes the topic of the technical
+            documentation.
+          </li>
+          <li>
+            Additionally, the navbar should contain link (<code>a</code>)
+            elements with the class of <code>nav-link</code>. There should be
+            one for every element with the class <code>main-section</code>.
+          </li>
+          <li>
+            The <code>header</code> element in the navbar must come before any
+            link (<code>a</code>) elements in the navbar.
+          </li>
+          <li>
+            Each element with the class of <code>nav-link</code> should contain
+            text that corresponds to the <code>header</code> text within each{" "}
+            <code>section</code> (e.g. if you have a "Hello world"
+            section/header, your navbar should have an element which contains
+            the text "Hello world").
+          </li>
+          <li>
+            When I click on a navbar element, the page should navigate to the
+            corresponding section of the <code>main-doc</code> element (e.g. If
+            I click on a <code>nav-link</code> element that contains the text
+            "Hello world", the page navigates to a <code>section</code> element
+            that has that id and contains the corresponding <code>header</code>.
+          </li>
+          <li>
+            On regular sized devices (laptops, desktops), the element with{" "}
+            <code>id="navbar"</code> should be shown on the left side of the
+            screen and should always be visible to the user.
+          </li>
+          <li>
+            My Technical Documentation page should use at least one media query.
+          </li>
+        </ol>
+      </Container>
+    </>
+  );
+};
