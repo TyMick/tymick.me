f9f9fedbcd8fd55caf51d7cd15e57b59baf3698c
Ty Mick <ty@tymick.me>
2020-02-03 10:27:08 -0600

    Move .nojekyll and CNAME to source

--- pages/project/personal-site.jsx
+++ pages/project/personal-site.jsx
@@ -11,7 +11,7 @@ const npmScripts = `"scripts": {
   "dev": "next dev",
   "build": "next build && next export",
   "start": "next start",
-  "predeploy": "npm run build && touch out/.nojekyll && echo 'tymick.me' >> out/CNAME",
+  "predeploy": "npm run build",
   "deploy": "gh-pages -b master -d out -m Build -t"
 }`;
 
