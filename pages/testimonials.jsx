fe8ad2a1331a24c96cbcadce2d42a3e6cb350289
Ty Mick <ty@tymick.me>
2020-04-15 12:11:38 -0400

    Add Nick's LinkedIn and title to testimonial

--- pages/testimonials.jsx
+++ pages/testimonials.jsx
@@ -12,7 +12,7 @@ export default () => (
     <Container className="cap-width-lg mb-n5">
       <h1 className="mb-4 mb-md-5">Testimonials</h1>
 
-      <Testimonial source="Nick Massie [on Upwork](https://www.upwork.com/o/profiles/users/~01e11c1caf833675be/)">
+      <Testimonial source="[Nick Massie](https://www.linkedin.com/in/massienick/), Founder & President of Beyond Social, [on Upwork](https://www.upwork.com/o/profiles/users/~01e11c1caf833675be/)">
         Tyler was a joy to work with. He suggested beneficial enhancements to
         our project that otherwise would not have been considered. Even more
         than his great programming skill, Tyler is an even better communicator.
