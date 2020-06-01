451177b1661ed1d0ce03f0df090637154b180f6a
Ty Mick <ty@tymick.me>
2020-06-01 19:44:23 -0400

    Add Coil monetization

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -25,6 +25,7 @@ export default ({ Component, pageProps }) => {
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <link href="https://fonts.googleapis.com/css?family=Alegreya|Dawning+of+a+New+Day&display=swap" rel="stylesheet" />
+        <meta name="monetization" content="$ilp.uphold.com/6NNW67MyBLDw" />
       </Head>
 
       <Nav />
