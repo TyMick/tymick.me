495822bc69d1a9ea9a045ec03f6b595e3a8fdbcf
Ty Mick <ty@tymick.me>
2020-06-13 15:55:38 -0400

    Switch to JS redirect with meta refresh fallback

--- components/client-side-redirect.jsx
+++ components/client-side-redirect.jsx
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Head from "next/head";
 import { Container } from "react-bootstrap";
 
-const Redirect = ({ href }) => (
-  <>
-    <Head>
-      <meta httpEquiv="refresh" content={`0;url=${href}`} />
-      <link rel="canonical" href={href} />
-    </Head>
-    <Container fluid as="p" style={{ textAlign: "left" }}>
-      Redirecting to <a href={href}>{href}</a>&hellip;
-    </Container>
-  </>
-);
+export default function Redirect({ href }) {
+  useEffect(() => {
+    window.location = href;
+  }, []);
 
-export default Redirect;
+  return (
+    <>
+      <Head>
+        <noscript>
+          <meta httpEquiv="refresh" content={`0;url=${href}`} />
+        </noscript>
+        <link rel="canonical" href={href} />
+      </Head>
+      <Container fluid as="p" style={{ textAlign: "left" }}>
+        Redirecting to <a href={href}>{href}</a>&hellip;
+      </Container>
+    </>
+  );
+}
