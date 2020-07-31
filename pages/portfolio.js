718010739e5f8c9e7156ca42b6fc9dafe35b6101
Ty Mick <ty@tymick.me>
2020-07-31 12:39:32 -0400

    Reword /portfolio for data science

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,284 +1,154 @@
 import React from "react";
 import Head from "next/head";
-import Link from "next/link";
 import { Container } from "react-bootstrap";
 import PortfolioSection from "../components/portfolio-section";
-import InlineFootnote from "../components/inline-footnote";
 
-const Portfolio = () => (
-  <>
-    <Head>
-      <title>Portfolio – Ty Mick</title>
-    </Head>
+export default function Portfolio() {
+  return (
+    <>
+      <Head>
+        <title>Portfolio – Ty Mick</title>
+      </Head>
 
-    <Container className="cap-width-lg">
-      <h1>Portfolio</h1>
+      <Container className="cap-width-lg">
+        <h1>Portfolio</h1>
 
-      <p>
-        You know what the{" "}
-        <InlineFootnote
-          note="I realize this is very subjective, but bear with me."
-          noteId="funPart"
-        >
-          fun part
-        </InlineFootnote>{" "}
-        of web development is? The <em>logic</em>. The{" "}
-        <em>behind-the-scenes</em>. The stuff you don&rsquo;t notice until
-        something&rsquo;s going wrong. <em>That&rsquo;s</em> where the
-        excitement&rsquo;s at, and that&rsquo;s where I prefer to spend my time.
-      </p>
-      <p>
-        That being said, while I&rsquo;m not nearly as interested in how
-        something looks than in how it works, I realize that the two concepts
-        are very intertwined. Good design <em>works better</em> than bad design.
-        I&rsquo;ve gotten to know my way around{" "}
-        <a href="https://getbootstrap.com/">Bootstrap</a> pretty well by now, so
-        I can definitely make something functional and attractive.
-      </p>
-      <p>
-        Hope you enjoy what I&rsquo;ve built, and{" "}
-        <Link href="/connect">
-          <a title="Connect – Ty Mick">let me know</a>
-        </Link>{" "}
-        if you have any questions!
-      </p>
+        <PortfolioSection
+          name="Published papers and articles"
+          id="papers-articles"
+          projects={[
+            <>
+              <a href="https://apps.dtic.mil/sti/citations/AD1012051">
+                Readiness Based Sparing: A Research Summary
+              </a>
+              <cite className="text-muted ml-3">
+                <abbr title="Defense Technical Information Center">DTIC</abbr>
+              </cite>
+            </>,
+            <>
+              <a href="https://apps.dtic.mil/sti/citations/AD1012101">
+                The Case for AFMC Social Media Analysis
+              </a>
+              <cite className="text-muted ml-3">DTIC</cite>
+            </>,
+            <>
+              <a href="https://hackernoon.com/making-a-browser-console-log-wait-until-page-reloads-a-how-to-guide-2j1z3yt9">
+                Making a Browser Console Log Wait Until Page Reloads [A How-To
+                Guide]
+              </a>
+              <cite className="text-muted ml-3">Hacker Noon</cite>
+            </>,
+          ]}
+        />
 
-      <PortfolioSection
-        name="Open-source contributions"
-        id="open-source"
-        projects={[
-          {
-            name: "Several contributions",
-            extra: " to Next.js",
-            href:
-              "https://github.com/vercel/next.js/pulls?q=author%3Atywmick+is%3Apr+is%3Amerged",
-          },
-          {
-            name: "New features",
-            extra: " for nextjs-redirect",
-            href: "https://github.com/pablopunk/nextjs-redirect/pulls?q=author%3Atywmick+is%3Apr+is%3Amerged",
-          },
-          {
-            name: "Curriculum help",
-            extra: " for freeCodeCamp",
-            href:
-              "https://github.com/freeCodeCamp/freeCodeCamp/pulls?q=author%3Atywmick+is%3Apr+is%3Amerged",
-          },
-          {
-            name: "Documentation help",
-            extra: " for Bootstrap",
-            href: "https://github.com/twbs/bootstrap/pull/30010",
-          },
-          {
-            name: "Documentation help",
-            extra: " for Ant Design",
-            href: "https://github.com/ant-design/ant-design/pull/23917",
-          },
-          {
-            name: "Documentation help",
-            extra: " for Code Shelter",
-            href:
-              "https://gitlab.com/codeshelter/codeshelter-web/-/merge_requests/10",
-          },
-        ]}
-      />
+        <PortfolioSection
+          name="Machine learning models"
+          id="machine-learning"
+          projects={[
+            <a href="https://www.kaggle.com/tywmick/building-a-neural-network-to-predict-loan-risk">
+              Loan risk prediction using neural network regression
+            </a>,
+          ]}
+        />
 
-      <PortfolioSection
-        name="Full-stack demos"
-        id="full-stack"
-        projects={[
-          {
-            name: "Server-rendered real estate listing page",
-            href: "https://real-estate-listing-page.tymick.me",
-          },
-          {
-            name: "Next.js local authentication boilerplate",
-            href: "https://nextjs-local-authentication.tymick.me",
-          },
-        ]}
-      />
+        <PortfolioSection
+          name="Python APIs and microservices"
+          id="python-apis"
+          projects={[
+            <a href="https://tywmick.pythonanywhere.com">
+              Loan risk prediction model API
+            </a>,
+            <a href="https://ty-metricimpconverter-python.glitch.me">
+              Metric-imperial converter
+            </a>,
+            <a href="https://ty-file-metadata-python.glitch.me">
+              File metadata microservice
+            </a>,
+            <a href="https://ty-timestamp-python.glitch.me">
+              Timestamp microservice
+            </a>,
+            <a href="https://ty-headerparser-python.glitch.me">
+              Request header parser
+            </a>,
+          ]}
+        />
 
-      <PortfolioSection
-        name="Multi-page static websites"
-        id="static-sites"
-        projects={[
-          {
-            name: "TyMick.me",
-            href: "/project/personal-site",
-            linkTitle: "About this site",
-          },
-        ]}
-      />
+        <PortfolioSection
+          name="Node.js APIs and microservices"
+          id="nodejs-apis"
+          projects={[
+            <a href="https://ty-library.glitch.me">Personal library</a>,
+            <a href="https://ty-stockchecker.glitch.me">Stock price checker</a>,
+            <a href="https://ty-issue-tracker.glitch.me">Issue tracker</a>,
+            <a href="https://ty-metricimpconverter.glitch.me">
+              Metric-imperial converter
+            </a>,
+            <a href="https://ty-messageboard.glitch.me">
+              Anonymous message board
+            </a>,
+            <a href="https://ty-exercise-tracker.glitch.me">
+              Exercise tracker
+            </a>,
+            <a href="https://ty-urlshortener.glitch.me">URL shortener</a>,
+            <a href="https://ty-file-metadata.glitch.me">
+              File metadata microservice
+            </a>,
+            <a href="https://ty-timestamp.glitch.me">Timestamp microservice</a>,
+            <a href="https://ty-headerparser.glitch.me">
+              Request header parser
+            </a>,
+          ]}
+        />
 
-      <PortfolioSection
-        name="Node.js API/microservice demos"
-        id="api-microservices"
-        projects={[
-          {
-            name: "Personal library",
-            href: "https://ty-library.glitch.me",
-          },
-          {
-            name: "Stock price checker",
-            href: "https://ty-stockchecker.glitch.me",
-          },
-          {
-            name: "Issue tracker",
-            href: "https://ty-issue-tracker.glitch.me",
-          },
-          {
-            name: "Metric-imperial converter",
-            href: "https://ty-metricimpconverter.glitch.me",
-          },
-          {
-            name: "Anonymous message board",
-            href: "https://ty-messageboard.glitch.me",
-          },
-          {
-            name: "Exercise tracker",
-            href: "https://ty-exercise-tracker.glitch.me",
-          },
-          {
-            name: "URL shortener",
-            href: "https://ty-urlshortener.glitch.me",
-          },
-          {
-            name: "File metadata",
-            href: "https://ty-file-metadata.glitch.me",
-          },
-          {
-            name: "Timestamp",
-            href: "https://ty-timestamp.glitch.me",
-          },
-          {
-            name: "Request header parser",
-            href: "https://ty-headerparser.glitch.me",
-          },
-        ]}
-      />
-
-<PortfolioSection
-        name="Python API/microservice demos"
-        id="python-apis"
-        projects={[
-          {
-            name: "Metric-imperial converter",
-            href: "https://ty-metricimpconverter-python.glitch.me",
-          },
-          {
-            name: "File metadata",
-            href: "https://ty-file-metadata-python.glitch.me",
-          },
-          {
-            name: "Timestamp",
-            href: "https://ty-timestamp-python.glitch.me",
-          },
-          {
-            name: "Request header parser",
-            href: "https://ty-headerparser-python.glitch.me",
-          },
-        ]}
-      />
-
-      <PortfolioSection
-        name="WordPress themes"
-        id="wordpress"
-        projects={[
-          {
-            name: "Personal site WordPress port",
-            href: "https://ty-mick-wordpress-port.000webhostapp.com",
-          },
-        ]}
-      />
-
-      <PortfolioSection
-        name="Single-page React apps"
-        id="react-apps"
-        projects={[
-          {
-            name: "Pomodoro clock",
-            href: "https://ty-pomodoro-clock.glitch.me",
-          },
-          {
-            name: "JavaScript calculator",
-            href: "https://codepen.io/tywmick/full/BaBVeVO",
-          },
-          {
-            name: "Drum machine",
-            href: "https://codepen.io/tywmick/full/zYOaPyW",
-          },
-          {
-            name: "Markdown previewer",
-            href: "https://codepen.io/tywmick/full/PoYeapB",
-          },
-        ]}
-      />
-
-      <PortfolioSection
-        name="React hooks"
-        id="react-hooks"
-        projects={[
-          {
-            name: "useWindowWidthBreakpoints",
-            href: "https://github.com/tywmick/use-window-width-breakpoints",
-          },
-        ]}
-      />
-
-      <PortfolioSection
-        name="Single-page PHP apps"
-        id="php"
-        projects={[
-          {
-            name: "To-do list",
-            href: "http://php-todo.tymick.me",
-          },
-        ]}
-      />
-
-      <PortfolioSection
-        name="JavaScript algorithms"
-        id="javascript-algorithms"
-        projects={[
-          {
-            name: "Roman numeral converter",
-            href: "/project/roman-numeral-converter",
-          },
-          {
-            name: "Cash register",
-            href: "/project/cash-register",
-          },
-          {
-            name: "Palindrome checker",
-            href: "/project/palindrome-checker",
-          },
-          {
-            name: "Telephone number validator",
-            href: "/project/telephone-number-validator",
-          },
-          {
-            name: "Caesar's cipher",
-            href: "/project/caesars-cipher",
-          },
-        ]}
-      />
-
-      <PortfolioSection
-        name="Single-page responsive web designs"
-        id="single-page-designs"
-        projects={[
-          {
-            name: "Technical documentation page",
-            href: "/project/technical-documentation-page",
-          },
-          {
-            name: "Product landing page",
-            href: "/project/product-landing-page",
-          },
-        ]}
-      />
-    </Container>
-  </>
-);
-
-export default Portfolio;
+        <PortfolioSection
+          name="Open-source contributions"
+          id="open-source"
+          projects={[
+            <>
+              <a href="https://github.com/pandas-dev/pandas/pull/35337">
+                Documentation help
+              </a>{" "}
+              for pandas
+            </>,
+            <>
+              <a href="https://github.com/freeCodeCamp/freeCodeCamp/pulls?q=author%3Atywmick+is%3Apr+is%3Amerged">
+                Curriculum help
+              </a>{" "}
+              for freeCodeCamp
+            </>,
+            <>
+              <a href="https://github.com/vercel/next.js/pulls?q=author%3Atywmick+is%3Apr+is%3Amerged">
+                Bug fixes and documentation help
+              </a>{" "}
+              for Next.js
+            </>,
+            <>
+              <a href="https://github.com/pablopunk/nextjs-redirect/pulls?q=author%3Atywmick+is%3Apr+is%3Amerged">
+                New features
+              </a>{" "}
+              for nextjs-redirect
+            </>,
+            <>
+              <a href="https://github.com/twbs/bootstrap/pull/30010">
+                Documentation help
+              </a>{" "}
+              for Bootstrap
+            </>,
+            <>
+              <a href="https://github.com/ant-design/ant-design/pull/23917">
+                Documentation help
+              </a>{" "}
+              for Ant Design
+            </>,
+            <>
+              <a href="https://gitlab.com/codeshelter/codeshelter-web/-/merge_requests/10">
+                Documentation help
+              </a>{" "}
+              for Code Shelter
+            </>,
+          ]}
+        />
+      </Container>
+    </>
+  );
+}
