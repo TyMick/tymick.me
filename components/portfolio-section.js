eeb98e0b761f166cd917ff266a4a5b0dd9188b40
Ty Mick <ty@tymick.me>
2020-08-10 14:24:13 -0400

    Adjust /portfolio formatting

--- components/portfolio-section.js
+++ components/portfolio-section.js
@@ -1,14 +1,19 @@
 import React from "react";
+import useWindowWidthBreakpoints from "use-window-width-breakpoints";
 
 export default function PortfolioSection({ name, id, projects }) {
+  const breakpoint = useWindowWidthBreakpoints();
+
   return (
     <section aria-labelledby={id}>
-      <h2 className="h3" id={id}>
+      <h2 className="h4" id={id}>
         {name}
       </h2>
-      <ul className="lead">
+      <ul style={{ ...(breakpoint.xs && { paddingInlineStart: "2rem" }) }}>
         {projects.map((project, index) => (
-          <li className="text-left" key={`${id}-${index}`}>{project}</li>
+          <li className="text-left" key={`${id}-${index}`}>
+            {project}
+          </li>
         ))}
       </ul>
     </section>
