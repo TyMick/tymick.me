29cdf9aafea9955f0f66e3a97e8ab2abe629b98b
Ty Mick <ty@tymick.me>
2020-09-10 14:55:50 -0400

    Self-host fonts

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,6 @@ export default class MyDocument extends Document {
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="manifest" href="/site.webmanifest" />
-          <link href="https://fonts.googleapis.com/css?family=Alegreya|Dawning+of+a+New+Day&display=swap" rel="stylesheet" />
           <meta name="monetization" content="$ilp.uphold.com/6NNW67MyBLDw" />
         </Head>
         <body>
