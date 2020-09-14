bbf250d850161ca3c797396d612d99787ce745a9
Ty Mick <ty@tymick.me>
2020-09-14 10:32:51 -0400

    Hyphenate justified text

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
+      <Html lang="en-US">
         {/* prettier-ignore */}
         <Head>
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
