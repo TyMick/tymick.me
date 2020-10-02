1ce377a41ccb644d11c6a58602bd1388b54d7f57
Ty Mick <ty@tymick.me>
2020-10-02 15:48:56 -0400

    Add new API projects

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -83,9 +83,18 @@ export default function Portfolio() {
           name="Machine learning models"
           id="machine-learning"
           projects={[
-            <a href="https://www.kaggle.com/tywmick/building-a-neural-network-to-predict-loan-risk">
-              Loan risk prediction using neural network regression
-            </a>,
+            <>
+              <a href="https://www.kaggle.com/tywmick/building-a-neural-network-to-predict-loan-risk">
+                Loan risk prediction using neural network regression
+              </a>
+              <ul>
+                <li>
+                  <a href="https://tywmick.pythonanywhere.com">
+                    Flask API serving this model&rsquo;s predictions
+                  </a>
+                </li>
+              </ul>
+            </>,
           ]}
         />
 
@@ -100,51 +109,123 @@ export default function Portfolio() {
         />
 
         <PortfolioSection
-          name="Python APIs and microservices"
-          id="python-apis"
+          name="APIs and microservices"
+          id="apis"
           projects={[
-            <a href="https://tywmick.pythonanywhere.com">
-              Loan risk prediction model API
-            </a>,
-            <a href="https://ty-metricimpconverter-python.glitch.me">
-              Metric-imperial converter
-            </a>,
-            <a href="https://ty-file-metadata-python.glitch.me">
-              File metadata microservice
-            </a>,
-            <a href="https://ty-timestamp-python.glitch.me">
-              Timestamp microservice
-            </a>,
-            <a href="https://ty-headerparser-python.glitch.me">
-              Request header parser
-            </a>,
-          ]}
-        />
-
-        <PortfolioSection
-          name="Node.js APIs and microservices"
-          id="nodejs-apis"
-          projects={[
-            <a href="https://ty-library.glitch.me">Personal library</a>,
-            <a href="https://ty-stockchecker.glitch.me">Stock price checker</a>,
-            <a href="https://ty-issue-tracker.glitch.me">Issue tracker</a>,
-            <a href="https://ty-metricimpconverter.glitch.me">
-              Metric-imperial converter
-            </a>,
-            <a href="https://ty-messageboard.glitch.me">
-              Anonymous message board
-            </a>,
-            <a href="https://ty-exercise-tracker.glitch.me">
-              Exercise tracker
-            </a>,
-            <a href="https://ty-urlshortener.glitch.me">URL shortener</a>,
-            <a href="https://ty-file-metadata.glitch.me">
-              File metadata microservice
-            </a>,
-            <a href="https://ty-timestamp.glitch.me">Timestamp microservice</a>,
-            <a href="https://ty-headerparser.glitch.me">
-              Request header parser
-            </a>,
+            <>
+              Python/Flask with SQLite
+              <ul className="mb-1">
+                <li>
+                  <a href="https://library-python.tymick.me">
+                    Personal library
+                  </a>
+                </li>
+                <li>
+                  <a href="https://stock-checker-python.tymick.me">
+                    Stock price checker
+                  </a>
+                </li>
+                <li>
+                  <a href="https://issue-tracker-python.tymick.me">
+                    Issue tracker
+                  </a>
+                </li>
+                <li>
+                  <a href="https://message-board-python.tymick.me">
+                    Anonymous message board
+                  </a>
+                </li>
+                <li>
+                  <a href="https://exercise-tracker-python.tymick.me">
+                    Exercise tracker
+                  </a>
+                </li>
+                <li>
+                  <a href="https://url-shortener-python.tymick.me">
+                    URL shortener
+                  </a>
+                </li>
+              </ul>
+            </>,
+            <>
+              Python/Flask
+              <ul className="mb-1">
+                <li>
+                  <a href="https://ty-metricimpconverter-python.glitch.me">
+                    Metric-imperial converter
+                  </a>
+                </li>
+                <li>
+                  <a href="https://ty-file-metadata-python.glitch.me">
+                    File metadata microservice
+                  </a>
+                </li>
+                <li>
+                  <a href="https://ty-timestamp-python.glitch.me">
+                    Timestamp microservice
+                  </a>
+                </li>
+                <li>
+                  <a href="https://ty-headerparser-python.glitch.me">
+                    Request header parser
+                  </a>
+                </li>
+              </ul>
+            </>,
+            <>
+              Node.js/Express with MongoDB
+              <ul className="mb-1">
+                <li>
+                  <a href="https://ty-library.glitch.me">Personal library</a>
+                </li>
+                <li>
+                  <a href="https://ty-stockchecker.glitch.me">
+                    Stock price checker
+                  </a>
+                </li>
+                <li>
+                  <a href="https://ty-issue-tracker.glitch.me">Issue tracker</a>
+                </li>
+                <li>
+                  <a href="https://ty-messageboard.glitch.me">
+                    Anonymous message board
+                  </a>
+                </li>
+                <li>
+                  <a href="https://ty-exercise-tracker.glitch.me">
+                    Exercise tracker
+                  </a>
+                </li>
+                <li>
+                  <a href="https://ty-urlshortener.glitch.me">URL shortener</a>
+                </li>
+              </ul>
+            </>,
+            <>
+              Node.js/Express
+              <ul>
+                <li>
+                  <a href="https://ty-metricimpconverter.glitch.me">
+                    Metric-imperial converter
+                  </a>
+                </li>
+                <li>
+                  <a href="https://ty-file-metadata.glitch.me">
+                    File metadata microservice
+                  </a>
+                </li>
+                <li>
+                  <a href="https://ty-timestamp.glitch.me">
+                    Timestamp microservice
+                  </a>
+                </li>
+                <li>
+                  <a href="https://ty-headerparser.glitch.me">
+                    Request header parser
+                  </a>
+                </li>
+              </ul>
+            </>,
           ]}
         />
 
