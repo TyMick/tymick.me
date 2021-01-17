2ae744cd59428b591f780051726811961a2a6c6d
Ty Mick <ty@tymick.me>
2021-01-17 14:14:31 -0600

    Rearrange types in MastodonVerification

--- components/MastodonVerification.tsx
+++ components/MastodonVerification.tsx
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { trimStart } from "lodash";
 
-type MastodonVerificationProps =
-  | { mastodonUrl?: never; instance: string; username: string }
-  | { mastodonUrl: string; instance?: never; username?: never };
+type Props =
+  | { instance: string; username: string; mastodonUrl?: never }
+  | { instance?: never; username?: never; mastodonUrl: string };
 
 export default function MastodonVerification({
   instance,
   username,
   mastodonUrl,
-}: MastodonVerificationProps) {
+}: Props) {
   const href =
     mastodonUrl ?? `https://${instance}/@${trimStart(username, "@")}`;
 
